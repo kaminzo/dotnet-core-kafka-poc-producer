3be71cdc64e43e21686e482130f25ff2ebd78f32
kaminzo <kaminroll@gmail.com>
2019-11-12 15:02:38 +0000

    changes

--- Program.cs
+++ Program.cs
@@ -1,31 +1,29 @@
-﻿namespace dotnet_core_kafka_poc_producer
-{
-    using System;
-    using System.Collections.Generic;
-    using System.Text;
-    using Confluent.Kafka;
+﻿using System;
+using Confluent.Kafka;
 
+namespace dotnet_core_kafka_poc_producer
+{
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            var config = new ProducerConfig { BootstrapServers = "localhost:9092"};
+            var conf = new ProducerConfig { BootstrapServers = "localhost:9092" };
 
-            Action<DeliveryReport<Null, string>> handler = r =>
+            Action<DeliveryReport<Null, string>> handler = r => 
                 Console.WriteLine(!r.Error.IsError
-                ? $"Delivered message to {r.TopicPartitionOffset}"
-                : $"Delivery Error: {r.Error.Reason}");
+                    ? $"Delivered message to {r.TopicPartitionOffset}"
+                    : $"Delivery Error: {r.Error.Reason}");
 
-            using (var producer = new ProducerBuilder<Null, string>(config).Build())
+            using (var p = new ProducerBuilder<Null, string>(conf).Build())
             {
-                var stringValue = "";
-                for (int i=0; i<100; ++i){
-                    stringValue += "🍌";
-                    producer.ProduceAsync("banana-topic", new Message<Null, string> { Value = stringValue});
+                for (int i=0; i<100; ++i)
+                {
+                    p.Produce("my-topic", new Message<Null, string> { Value = i.ToString() }, handler);
                 }
 
-                producer.Flush(TimeSpan.FromSeconds(10));
-            }    
+                // wait for up to 10 seconds for any inflight messages to be delivered.
+                p.Flush(TimeSpan.FromSeconds(10));
+            }
         }
     }
 }
